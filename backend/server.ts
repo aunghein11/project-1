33895e8c7979fc5b20f98af2f129492d5f10642e
Aung Hein <heina8649@example.com>
2024-04-02 11:55:05 +0630

    start project 1th day

---
+++ backend/server.ts
@@ -0,0 +1,15 @@
+import cors from "cors";
+import express from "express";
+
+const app = express();
+const port = 5006;
+app.use(cors());
+app.use(express.json());
+
+app.post("/auth/register", async (req, res) => {
+  const { name, email, password } = req.body;
+
+  res.send({ name, email, password });
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
